3badc5c538ed1cca8226ac790a43ae28ffbbcaf6
HoRichard <richardho16@hotmail.com>
2025-10-04 22:17:04 -0700

    implemented get and set for player in game manager

--- pixi-project/src/managers/GameManager.ts
+++ pixi-project/src/managers/GameManager.ts
@@ -1,21 +1,37 @@
 import { BoardManager } from "./BoardManager";
 import { DangerManager } from "./DangerManager";
+import { LocalStorageDataSource } from "../player/LocalStorageDataSource";
+
+interface Player {
+  id: string;
+  name: string;
+  hitpoints: number;
+  score: number;
+}
 
 export class GameManager {
 
   #boardManager: BoardManager
   #dangerManager: DangerManager
+  #playerSource: LocalStorageDataSource<Player>;
 
   constructor() {
     this.#boardManager = new BoardManager();
     this.#dangerManager = new DangerManager();
+    this.#playerSource = new LocalStorageDataSource<Player>('players');
+
   }
 
   getPlayerHp(): number {
-    throw new Error("not implemented");
+    const playerData = this.#playerSource.getById('hitpoints');
+    return playerData?.hitpoints ?? 0;
   }
 
-  setPlayerHp(hp : number): void {
-
+  setPlayerHp(playerId: number, hp : number): void {
+    const playerData = this.#playerSource.getById(playerId);
+    if (playerData) {
+      playerData.hitpoints = hp;
+      this.#playerSource.update(playerId, playerData);
+    }
   }
 }
